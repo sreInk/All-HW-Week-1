b1c8ec2a25db3ab44cad3536af66cf809d5cba31
sreInk <srinknok@gmail.com>
2024-10-15 08:35:45 +0530

    f

---
+++ hw6.py
@@ -0,0 +1,19 @@
+import math
+
+def tv():
+    
+    angle_deg = float(input("Enter the angle in degrees: "))
+    
+    angle_rad = math.radians(angle_deg)
+    
+    
+    sin_value = math.sin(angle_rad)
+    cos_value = math.cos(angle_rad)
+    tan_value = math.tan(angle_rad)
+    
+   
+    print(f"sin({angle_deg}°) = {sin_value}")
+    print(f"cos({angle_deg}°) = {cos_value}")
+    print(f"tan({angle_deg}°) = {tan_value}")
+
+tv()
